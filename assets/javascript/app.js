15ff69a6c8dfbf939b323d32a5b8ce969941c4e3
Joshua Beckford <joshua93beckford@gmail.com>
2018-07-06 12:15:50 -0400

    V 0.0.1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -52,57 +52,120 @@ var questions = [
         answer: "/"
     }
 ]
+var correct = 0;
+var incorrect = 0;
+var counter = 90;
+var timer;
+function countdown() {
+    counter--;
+    $("#counter").html(counter);
+    if (counter <= 0) {
+        done();
+    }
+}
+function start() {
+    timer = setInterval(countdown, 1000);
+    $("#content").prepend('<h2> Time Remaining: <span id="counter">' + counter + '</span> Seconds </h2>')
+    $("#button").remove();
+    for (var i = 0; i < questions.length; i++) {
+        $("#content").append("<h4>" + questions[i].question + "</h4>");
 
+        for (var j = 0; j < questions[i].choices.length; j++) {
 
+            $("#content").append("<input type ='radio' name ='question-'" + i + "' value='" + questions[i].choices[j] + "'>" + questions[i].choices[j]);
 
-var startGame;
-
-$(document).ready(function () {
-    $("#quizPage").hide();
-    $("#scorePage").hide();
-});
+        }
+    }
+    $("#content").append("<button onclick='done()'>Done</button>")
+}
+function done() {
+    $.each($('input[name="question-0"]:checked'), function () {
+        if ($(this).val() == questions[0].correctAnswer) {
+            correct++;
+        } else {
+            incorrect++;
+        }
+    });
+    $.each($('input[name="question-1"]:checked'), function () {
+        if ($(this).val() == questions[1].correctAnswer) {
+            correct++;
+        } else {
+            incorrect++;
+        }
+    });
+    $.each($('input[name="question-2"]:checked'), function () {
+        if ($(this).val() == questions[2].correctAnswer) {
+            correct++;
+        } else {
+            incorrect++;
+        }
+    });
+    $.each($('input[name="question-3"]:checked'), function () {
+        if ($(this).val() == questions[3].correctAnswer) {
+            correct++;
+        } else {
+            incorrect++;
+        }
+    });
+    $.each($('input[name="question-4"]:checked'), function () {
+        if ($(this).val() == questions[4].correctAnswer) {
+            correct++;
+        } else {
+            incorrect++;
+        }
+    });
+    $.each($('input[name="question-5"]:checked'), function () {
+        if ($(this).val() == questions[5].correctAnswer) {
+            correct++;
+        } else {
+            incorrect++;
+        }
+    });
+    $.each($('input[name="question-6"]:checked'), function () {
+        if ($(this).val() == questions[6].correctAnswer) {
+            correct++;
+        } else {
+            incorrect++;
+        }
+    });
+    $.each($('input[name="question-7"]:checked'), function () {
+        if ($(this).val() == questions[7].correctAnswer) {
+            correct++;
+        } else {
+            incorrect++;
+        }
+    });
+    $.each($('input[name="question-8"]:checked'), function () {
+        if ($(this).val() == questions[8].correctAnswer) {
+            correct++;
+        } else {
+            incorrect++;
+        }
+    });
+    $.each($('input[name="question-9"]:checked'), function () {
+        if ($(this).val() == questions[9].correctAnswer) {
+            correct++;
+        } else {
+            incorrect++;
+        }
+    });
+    result();
+}
+function result() {
+    clearInterval(timer);
+    $("#content h4").remove();
+    $("#content").html("<h2>Finished</h2>");
+    $("#content").append("<h3>Correct Answers: " + correct + "</h3>");
+    $("#content").append("<h3>Incorrect Answers: " + incorrect + "</h3>");
+    $("#content").append("<h3>Unanswered Questions: " + (questions.length - (incorrect + correct)) + "</h3>");
+    $("#content").append('<input type="button" value="Play Again" onClick="window.location.reload()">');
+}
 
 $("#button").on("click", function () {
 
-    startGame = setTimeout(endGame, 30000);
-    $("#startPage").hide();
-    $("#quizPage").show();
-    for (var i = 0; i < questions.length; i++) {
+    start();
 
-        var tempQuestion = $("<h4>" + questions[i].question + "</h4>");
-        tempQuestion.css("clear", "both");
-        tempQuestion.appendTo('#showQ');
 
-        for (var j = 0; j < questions[i].choices.length; j++) {
-
-            var tempChoices = $("<input>");
-            tempChoices.attr("type", "radio");
-            tempChoices.attr("name", i + " " + j);
-            tempChoices.attr("value", questions[i].choices[j]);
-            tempChoices.css("float", "left");
-            tempChoices.appendTo('#showQ');
-            var tempTXT = $("<p>" + questions[i].choices[j] + "</p>");
-            tempTXT.css("float", "left");
-            tempTXT.appendTo('#showQ');
-        }
-    }
 });
 
-function endGame() {
-
-    $("#quizPage").hide();
-    $("#scorePage").show();
-
-    $("#btn").on("click", function () {
-        $("#scorePage").hide();
-        $("#startPage").show();
-    });
-
-    for (var i = 0; i < questions.length; i++) {
-
-        for (var j = 0; j < questions[i].choices.length; j++) {
-        }
-    }
-
-}
 
